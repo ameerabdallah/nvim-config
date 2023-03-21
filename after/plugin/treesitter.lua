1d9c1fadbac1dabcdde64349afa204299f7c3f63
Ameer Abdallah <ameeryabdallah1@gmail.com>
2023-03-20 20:02:43 -0700

    fix bash treesitter parser

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,24 +1,48 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "query", "rust", "javascript", "java", "typescript" },
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.bash_master = {
+    install_info = {
+        url = "https://github.com/tree-sitter/tree-sitter-bash",
+        files = { "src/parser.c", "src/scanner.cc" },
+        generate_requires_npm = true,
+        requires_generate_from_grammar = false,
+        branch = "master"
+    },
+    filetype = { "bash", "sh" }
+}
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+vim.treesitter.language.register("bash_master", "bash")
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "help",
+        "query",
+        "rust",
+        "javascript",
+        "java",
+        "typescript",
+        "bash_master"
+    },
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  highlight = {
-    enable = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+    highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
