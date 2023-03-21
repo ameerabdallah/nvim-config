d28a17afaac74744be3b593a2bea7f19c26bfe30
Ameer Abdallah <ameeryabdallah1@gmail.com>
2023-03-20 22:06:37 -0700

    Added copilot remappings

--- lua/aabdallah/remap.lua
+++ lua/aabdallah/remap.lua
@@ -41,3 +41,7 @@ end)
 
 --- My Remaps
 vim.keymap.set("n", "<leader>v", "<C-q>")
+
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Previous()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Next()', { silent = true, expr = true })
