42838366c02e85e850a9fe4688c7daa9acf848aa
Ameer Abdallah <ameeryabdallah1@gmail.com>
2023-03-28 11:36:21 -0700

    wsl system yank/paste support

--- lua/aabdallah/set.lua
+++ lua/aabdallah/set.lua
@@ -32,8 +32,21 @@ vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
-
-
+if vim.fn.has("win32") == 1 or vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = "unnamedplus"
+    vim.g.clipboard = {
+        name = "win32yank-wsl",
+        copy = {
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
+        },
+        cache_enabled = 0,
+    }
+end
 
 
 
