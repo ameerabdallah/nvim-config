93291ff4c6d5e25be5d21a9d835cd894ea85a39b
Ameer Abdallah <ameeryabdallah1@gmail.com>
2023-04-15 02:58:08 -0700

    added disable functionality for treesitter if file size is too large (prevents nvim locking up)

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -40,10 +40,14 @@ require 'nvim-treesitter.configs'.setup {
 
     highlight = {
         enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
+        -- Use function to determine if a file should be highlighted. Depends on file size.
+        disable = function(_, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
     },
 }
